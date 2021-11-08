5ed32ae1a12dfead312903373a3348d4a66afb02
Nate Higgers <SkrytnayaJaba@yandex.ru>
2021-11-08 21:24:09 +0400

    First Graphic Build
    Gotta Polish the look

--- src/keygen.py
+++ src/keygen.py
@@ -1,29 +1,71 @@
 import string 
 import random 
+from tkinter import *
 
 
-length = int(input())
-password = []
+#Параметры окна
+window = Tk()
+window.title("Keygen")
+window.geometry('400x200')
+
+
+#Заголовок
+key_length = Label(window, text="Введите длину пароля")
+key_length.pack()
+
+
+#Получаем входные данные 
+user_input = Entry(window, width=10)
+user_input.pack()
+user_input.focus()
+
+
+#Вводим пока пустую переменную для пароля
+final_key = Label(window, text="1")
+final_key.pack()
+
+
+password_arr = []
 symbols = []
 
 
-#Получаем массив с символами
-for symbol in string.printable:
-    symbols.append(symbol)
-    
-del symbols[95:100]
+#Логическая часть
+def Clicked():
 
 
-#Выбираем случайные символы из массива 
-def Random_Symbols():
-
-	for i in range (length+1):
-		secure_random = random.SystemRandom()
-		password.append(secure_random.choice(symbols))
-
-	return(''.join(password))
+	#Переводим входные данные в нужный формат
+	length = int(user_input.get())	
 
 
+	#Получаем массив с символами
+	for symbol in string.printable:
+		symbols.append(symbol)
 
-password = Random_Symbols()
-print(password)	
+
+	#Убираем лишние
+	del symbols[95:100]
+	del symbols[86]
+
+
+
+	#Выбираем случайные символы из массива 
+	def Random_Symbols():
+
+		for i in range (length+1):
+			secure_random = random.SystemRandom()
+			password_arr.append(secure_random.choice(symbols))
+
+		return(''.join(password_arr))
+
+
+	#Присваиваем пустой переменной парольное значение
+	password = Random_Symbols()
+	final_key.config(text = password)	
+
+
+#Пишем кнопулю, которая запускает весь цирк
+button = Button(window, text="Генерировать", command = Clicked)
+button.pack()
+
+#Продолжаем цикл окна(чтобы его не украли)
+window.mainloop()
