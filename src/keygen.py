e3ae8018088829ea3ad52993d6a840018e51f5fe
Nate Higgers <SkrytnayaJaba@yandex.ru>
2021-11-16 19:05:11 +0400

    Lazy fix
    It's Evolving, Just Backwards

--- src/keygen.py
+++ src/keygen.py
@@ -21,7 +21,9 @@ user_input.focus()
 
 
 #Вводим пока пустую переменную для пароля
-final_key = Label(window, text="1")
+final = StringVar()
+final.set('')
+final_key = Entry(window, textvariable=final)
 final_key.pack()
 
 
@@ -51,16 +53,17 @@ def Clicked():
 	#Выбираем случайные символы из массива 
 	def Random_Symbols():
 
-		for i in range (length+1):
+		for i in range (length):
 			secure_random = random.SystemRandom()
 			password_arr.append(secure_random.choice(symbols))
 
 		return(''.join(password_arr))
 
 
-	#Присваиваем пустой переменной парольное значение
+	#Присваиваем пустой переменной парольное значение	
 	password = Random_Symbols()
-	final_key.config(text = password)	
+	final_key.delete(0, 'end')
+	final.set(password)	
 
 
 #Пишем кнопулю, которая запускает весь цирк
